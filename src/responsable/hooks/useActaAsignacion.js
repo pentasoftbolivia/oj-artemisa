31f6f0d8adc1b4579474e4da07f858270118a5bb
Javier <javiercostasaliaga@gmail.com>
2026-09-03 12:41:28 -0400

    En estado inventariado para Acta de Asignacion

--- src/responsable/hooks/useActaAsignacion.js
+++ src/responsable/hooks/useActaAsignacion.js
@@ -43,7 +43,7 @@ export const loadActaData = async (responsable, locationFilters = {}) => {
     .select(ASSET_SELECT)
     .eq("cirun", responsable.cirun)
     .eq("ultimoregistro", 1)
-    .eq("estadoinventario", "REVISADO");
+    .eq("estadoinventario", "INVENTARIADO");
 
   if (hasLocation) {
     const codes = await resolveAmbienteCodes({ ciudad, inmueble, nivel, ambiente });
